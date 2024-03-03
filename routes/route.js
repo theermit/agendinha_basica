82a997ff542c78bdf2f36571f16a0cdd20497825
benhur alencar azevedo <benhur.azevedo@hotmail.com>
2024-03-02 22:53:08 -0300

    configuracao de rotas com middleware de logado

--- routes/route.js
+++ routes/route.js
@@ -5,25 +5,27 @@ const contatoController = require("../controllers/contatoController");
 
 const contato_inc_edit_middleware = require("../middlewares/contatoMiddleware");
 
+const logado_middleware = require("../middlewares/logadoMiddleware");
+
 //rotas
 //index -> lista de contatos
-router.get("/lista", contatoController.exibir_lista);
+router.get("/lista", logado_middleware, contatoController.exibir_lista);
 
 //novo -> novo contato
-router.get("/novo", contatoController.exibir_form_novo);
+router.get("/novo", logado_middleware, contatoController.exibir_form_novo);
 
 // processar_incluir_novo_contato
-router.post("/incluir_contato", contato_inc_edit_middleware, contatoController.processar_incluir_novo_contato);
+router.post("/incluir_contato", logado_middleware, contato_inc_edit_middleware, contatoController.processar_incluir_novo_contato);
 
 //editar -> edita contato
-router.get("/editar/:contato_id", contatoController.exibir_form_editar);
+router.get("/editar/:contato_id", logado_middleware, contatoController.exibir_form_editar);
 
 // processar_editar_contato
-router.post("/editar_contato", contato_inc_edit_middleware, contatoController.processar_editar_contato);
+router.post("/editar_contato", logado_middleware, contato_inc_edit_middleware, contatoController.processar_editar_contato);
 
 //apagar -> apaga contato
-router.get("/apagar/:contato_id", contatoController.exibir_form_apagar_contato);
+router.get("/apagar/:contato_id", logado_middleware, contatoController.exibir_form_apagar_contato);
 
-router.post("/apagar_contato", contatoController.processar_apagar_contato);
+router.post("/apagar_contato", logado_middleware, contatoController.processar_apagar_contato);
 
 module.exports = router;
